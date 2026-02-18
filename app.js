14de113b834205374c47534a6bd41078ae7eda1a
shakibul-isalm-dev <ishakibul704@gmail.com>
2026-02-18 23:02:45 +0600

    agian

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log('Apps Are working')
+
+const logButton = document.getElementById('login-button')
+//addEventListener
+logButton.addEventListener('click', function () {
+  //User Id Grabbing
+  const userNumber = document.getElementById('user-number')
+  const userPin = document.getElementById('user-pin')
+  //userNumber
+  const contactNumber = userNumber.value
+  const pin = userPin.value
+
+  //userPin
+  if (contactNumber === '01617897307' && pin === '1234567') {
+    //Home page Linking
+    window.location.assign('/home.html')
+  } else {
+    alert('Loging Failed')
+    return
+  }
+  //true of false
+  console.log(contactNumber)
+  console.log(pin)
+})
